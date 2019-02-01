7055c09a5877a7f0ada8bdf6defc16d78983b39c
wkwksama <mailpakgasing@gmail.com>
2019-02-01 14:58:10 +0700

    test socketio

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,145 +1,79 @@
 <template>
   <v-container>
     <v-layout
-      text-xs-center
       wrap
     >
-      <v-flex xs12>
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        ></v-img>
-      </v-flex>
-
-      <v-flex mb-4>
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
-        </h1>
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank">Discord Community</a>
-        </p>
-      </v-flex>
-
-      <v-flex
-        mb-5
-        xs12
-      >
-        <h2 class="headline font-weight-bold mb-3">What's next?</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-layout>
-      </v-flex>
-
-      <v-flex
-        xs12
-        mb-5
-      >
-        <h2 class="headline font-weight-bold mb-3">Important Links</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-layout>
-      </v-flex>
-
-      <v-flex
-        xs12
-        mb-5
-      >
-        <h2 class="headline font-weight-bold mb-3">Ecosystem</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-layout>
-      </v-flex>
+      <h1 class="display-2 font-weight-bold mb-3">
+        test socket
+        <v-chip dark :color="connected ? 'success' : 'error'">{{ connected ? 'connected' : 'not connected' }}</v-chip>
+      </h1>
     </v-layout>
+    <v-layout>
+      <v-text-field
+        v-model="url"
+        label="URL"
+      />
+      <v-btn color="info" @click="connect">connect</v-btn>
+    </v-layout>
+    <v-divider></v-divider>
+    <v-layout>
+      <v-text-field
+        v-model="topic"
+        label="Subscribe topic"
+      />
+      <v-btn @click="subs" color="info">subscribe</v-btn>
+    </v-layout>
+    <v-layout>
+      <v-card color="grey" full-width>
+        <v-card-text>
+          <p v-for="(d, index) in data" :key="index">{{ d }}</p>
+        </v-card-text>
+      </v-card>
+    </v-layout>
+    <v-divider></v-divider>
   </v-container>
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      ecosystem: [
-        {
-          text: 'vuetify-loader',
-          href: 'https://github.com/vuetifyjs/vuetify-loader'
-        },
-        {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify'
-        },
-        {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify'
-        }
-      ],
-      importantLinks: [
-        {
-          text: 'Documentation',
-          href: 'https://vuetifyjs.com'
-        },
-        {
-          text: 'Chat',
-          href: 'https://community.vuetifyjs.com'
-        },
-        {
-          text: 'Made with Vuetify',
-          href: 'https://madewithvuetifyjs.com'
-        },
-        {
-          text: 'Twitter',
-          href: 'https://twitter.com/vuetifyjs'
-        },
-        {
-          text: 'Articles',
-          href: 'https://medium.com/vuetify'
-        }
-      ],
-      whatsNext: [
-        {
-          text: 'Explore components',
-          href: 'https://vuetifyjs.com/components/api-explorer'
-        },
-        {
-          text: 'Select a layout',
-          href: 'https://vuetifyjs.com/layout/pre-defined'
-        },
-        {
-          text: 'Frequently Asked Questions',
-          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions'
-        }
+import io from 'socket.io-client'
 
-      ]
-    })
+let socket
+
+export default {
+  data: () => ({
+    url: 'localhost:4000',
+    topic: 'blood',
+    connected: false,
+    data: []
+  }),
+
+  methods: {
+    connect () {
+      socket = io(this.url)
+      console.log('connecting...')
+
+      socket.on('connect', () => {
+        console.log('terhubung socket', socket)
+        this.connected = true
+      })
+
+      socket.on('disconnect', () => {
+        console.error('socket disconnected')
+        this.connected = false
+      })
+      socket.on('reconnect_failed', () => {
+        console.error('reconnect failed')
+        this.connected = false
+      });
+    },
+    subs () {
+      socket.on(this.topic, (data) => {
+        console.log('subscribed', data)
+        this.data.unshift(data)
+      })
+    },
   }
+}
 </script>
 
 <style>
